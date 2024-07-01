4dbd5efcd2e52ff08c39a4e9d1d8338d814a8c65
joseaugusto <joseaugusto0242@gmail.com>
2024-07-01 10:11:57 -0300

    init: add codigo

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")()
+
+while (true) {
+
+    let numero = prompt("Digite um número: ")
+
+    if (isNaN(Number(numero))) {
+
+        console.log("Número inválido")
+    } else {
+        numero = numero.split("")
+
+        for (let i = 0; i < numero.length; i++) {
+            for (let j = i + 1; j < numero.length; j++) {
+                if (numero[i] < numero[j]) {
+                    let aux = numero[i]
+                    numero[i] = numero[j]
+                    numero[j] = aux
+                }
+            }
+        }
+        console.log(numero.join(""))
+    }
+}
